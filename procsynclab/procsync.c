44378d309b20d8bfc1f1560dae59267134fcd9b2
Miical <jason.ljcer@gmail.com>
2023-05-24 17:40:59 +0800

    fix bug

--- procsynclab/procsync.c
+++ procsynclab/procsync.c
@@ -5,11 +5,12 @@
 #include <pthread.h>
 #include <sys/_pthread/_pthread_t.h>
 #include <semaphore.h>
+#include <sys/fcntl.h>
 #include <sys/semaphore.h>
 
 #define PRODUCER_NUM 3
 #define CONSUMER_NUM 3
-#define MAX_ITEMS 1
+#define MAX_ITEMS 5
 #define ITEMS_UNIT 18
 
 typedef struct {
@@ -27,16 +28,15 @@ int consumer_items[CONSUMER_NUM][ITEMS_UNIT], consumer_num[CONSUMER_NUM];
 void *producer_func(void *arg) {
     uint64_t producer_id = (uint64_t) arg;
     for (int i = 0; i < ITEMS_UNIT; i++) {
-        if (!sem_wait(items_list.slots)) exit(EXIT_FAILURE);
-        if (!sem_wait(items_list.mutex)) exit(EXIT_FAILURE);
+        if (sem_wait(items_list.slots)) exit(EXIT_FAILURE);
+        if (sem_wait(items_list.mutex)) exit(EXIT_FAILURE);
 
         items_list.buf[(++items_list.rear) % MAX_ITEMS] = ++items_list.item_cnt;
         producer_items[producer_id][producer_num[producer_id]++] = items_list.item_cnt;
         printf("Producer %lld put item %d\n", producer_id, items_list.item_cnt);
-        printf("rear = %d, front = %d\n", items_list.rear, items_list.front);
 
-        if (!sem_post(items_list.mutex)) exit(EXIT_FAILURE);
-        if (!sem_post(items_list.items)) exit(EXIT_FAILURE);
+        if (sem_post(items_list.mutex)) exit(EXIT_FAILURE);
+        if (sem_post(items_list.items)) exit(EXIT_FAILURE);
     }
     return NULL;
 }
@@ -44,16 +44,15 @@ void *producer_func(void *arg) {
 void *consumer_func(void *arg) {
     uint64_t consumer_id = (uint64_t) arg;
     for (int i = 0; i < ITEMS_UNIT; i++) {
-        if (!sem_wait(items_list.items)) exit(EXIT_FAILURE);
-        if (!sem_wait(items_list.mutex)) exit(EXIT_FAILURE);
+        if (sem_wait(items_list.items)) exit(EXIT_FAILURE);
+        if (sem_wait(items_list.mutex)) exit(EXIT_FAILURE);
 
         int item = items_list.buf[(++items_list.front) % MAX_ITEMS];
         consumer_items[consumer_id][consumer_num[consumer_id]++] = item;
         printf("Consumer %lld get item %d\n", consumer_id, item);
-        printf("rear = %d, front = %d\n", items_list.rear, items_list.front);
 
-        if (!sem_post(items_list.mutex)) exit(EXIT_FAILURE);
-        if (!sem_post(items_list.slots)) exit(EXIT_FAILURE);
+        if (sem_post(items_list.mutex)) exit(EXIT_FAILURE);
+        if (sem_post(items_list.slots)) exit(EXIT_FAILURE);
     }
     return NULL;
 }
@@ -63,9 +62,18 @@ int main() {
     pthread_t producer[PRODUCER_NUM];
 
     // Initialize semaphore.
-    items_list.slots = sem_open("slots", O_CREAT|O_EXCL, S_IRWXU, MAX_ITEMS);
-    items_list.mutex = sem_open("mutex", O_CREAT|O_EXCL, S_IRWXU, 1);
-    items_list.items = sem_open("items", O_CREAT|O_EXCL, S_IRWXU, 0);
+    sem_unlink("slots");
+    sem_unlink("mutex");
+    sem_unlink("items");
+    if ((items_list.slots = sem_open("slots", O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, MAX_ITEMS)) == SEM_FAILED) {
+        fprintf(stderr, "Semaphore Error.\n"); exit(EXIT_FAILURE);
+    }
+    if ((items_list.mutex = sem_open("mutex", O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, 1)) == SEM_FAILED) {
+        fprintf(stderr, "Semaphore Error.\n"); exit(EXIT_FAILURE);
+    }
+    if ((items_list.items = sem_open("items", O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, 0)) == SEM_FAILED) {
+        fprintf(stderr, "Semaphore Error.\n"); exit(EXIT_FAILURE);
+    }
 
     // Create producer and consumer threads.
     for (int64_t i = 0; i < PRODUCER_NUM; i++) {
@@ -112,6 +120,9 @@ int main() {
         printf("\n");
     }
 
+    sem_close(items_list.slots);
+    sem_close(items_list.mutex);
+    sem_close(items_list.items);
     sem_unlink("slots");
     sem_unlink("mutex");
     sem_unlink("items");
